98b8d72b59bae03e05edff30f3c4032b1b1599b3
Joonatan Laato <jlaato@gmail.com>
2021-06-18 19:36:55 +0300

    trying to get this working

--- Osa 1/kurssitiedot/src/App.js
+++ Osa 1/kurssitiedot/src/App.js
@@ -17,7 +17,8 @@ const Content = (props) => {
         {props.course.parts[0].name} {props.course.parts[0].exercises}
         <p>
         {props.course.parts[1].name} {props.course.parts[1].exercises}
-        </p> <p>
+        </p> 
+        <p>
         {props.course.parts[2].name} {props.course.parts[2].exercises}
         </p>
       </p>
